57903c0310b75fd75e4dd0c319d34b040bcd2a25
Ammar Ali <86957756+aamin75@users.noreply.github.com>
2023-11-11 22:17:41 +0100

    Change the name Die.java to Dice.java and more
    The file name was changed to correspond with their functions.
    An unused function was marked by a block comment.
    A function name was changed according to Java rules.

--- src/main/java/edu/lernia/labb5/Dice.java
+++ src/main/java/edu/lernia/labb5/Dice.java
@@ -1,20 +1,21 @@
 package edu.lernia.labb5;
 
-public class Die extends BoardGameMaterial {
+public class Dice extends BoardGameMaterial {
     public int value = 0;
 
-    public Die() {
+    public Dice() {
         value = (int) Math.random();
     }
 
-    public int DieRoll() {
+    public int diceRoll() {
         value = (int)(Math.random()*6+1);
         return value;
     }
 
-    public int DieReroll() {
-        return DieRoll();
-    }
+    // this function never used, I will not keep on it?
+	/*
+	 * public int DieReroll() { return DieRoll(); }
+	 */
 
     public String getString() {
         return "Dice shows " + value;
