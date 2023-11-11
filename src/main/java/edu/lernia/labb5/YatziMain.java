a05b07fef32d161adcb434efcd584c71f640dcdc
Ammar Ali <86957756+aamin75@users.noreply.github.com>
2023-11-11 22:11:23 +0100

    Change variables names and functions
    The names of the variables were changed to be more readable and understandable.
    The code was turned into a set of functions where everyone has a reasonable duty. So the code can be read and understood as simply as possible.

--- src/main/java/edu/lernia/labb5/YatziMain.java
+++ src/main/java/edu/lernia/labb5/YatziMain.java
@@ -3,60 +3,83 @@ package edu.lernia.labb5;
 import java.util.Scanner;
 
 public class YatziMain {
-    public static Die[] ds;
-    public static boolean bGameIsOn = true;
+    public static Dice[] dice;
+    public static boolean beginGameIsOn = true;
+    public static int iteration; 
+    public static int maxIter = 3;
+    public static int diceNum = 5; 
+    
 
-    public static void main(String[] args) {
-        int iturn = 0;
-        ds = new Die[5];
-        for(int d=0;d<5;d++) {
-            ds[d] = new Die();
+    private static void runRound() {
+    	for(int i=0;i<dice.length;i++) {
+            dice[i].diceRoll();
+            //ds[i].value = 5; //Test if yatzi work
+            System.out.println(i + ": " + dice[i].getString());
         }
+    }
+    
+    public static boolean isYATZI() {
+    	boolean flag = true; 
+    	for(int j=1;j<dice.length;j++) {
+             if(dice[j].value!=dice[j-1].value) {
+                 //Set flag to false
+                 flag = false;
+             }
+         }
+         return flag;
+    }
+    
+    public static void initiateDice() {
+    	dice = new Dice[diceNum];
+        for(int i=0;i<dice.length;i++) {
+            dice[i] = new Dice();
+        }
+    }
+    
+    private static void playGame() {
         //We will continue until the game is over
-        while(bGameIsOn == true) {
-            iturn = 0;
+        while(beginGameIsOn == true) {
+            iteration = 0;
             System.out.println("Welcome to Yatzi!");
-            while(iturn < 3) {
-                System.out.println("Starting turn " + (iturn+1) + " of 3, rolling dice.");
-                for(int i=0;i<ds.length;i++) {
-                    ds[i].DieRoll();
-                    //ds[i].value = 5; //Test if yatzi work
-                    System.out.println(i + ": " + ds[i].getString());
-                }
+            while(iteration < maxIter) {
+                System.out.println("Starting turn " + (iteration+1) + " of " + maxIter + ", rolling dice.");
+                runRound();
                 //YATZI
-                boolean flag = true;
-                for(int j=1;j<5;j++) {
-                    if(ds[j].value!=ds[j-1].value) {
-                        //Set flag to false
-                        flag = false;
-                    }
-                }
+                boolean flag = isYATZI();
+                
                 if(flag == true) {
-                    System.out.println("You got YATZI! in " + ds[0].value + "'s");
+                    System.out.println("You got YATZI! in " + dice[0].value + "'s");
                     return;
                 } else {
                     //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
-                    if(iturn != 2) {
+                    if(iteration != maxIter-1) {
                         System.out.println("Want to throw again? (y for yes, anything else for no)");
                         Scanner sc = new Scanner(System.in);
                         if(sc.next().equals("y")) {
-                            ++iturn;
+                            ++iteration;
                         } else {
-                            bGameIsOn = !bGameIsOn;
+                            beginGameIsOn = !beginGameIsOn;
                             break;
                         }
                     } else {
                         System.out.println("Game over! Want to play again?");
                         Scanner sc = new Scanner(System.in);
                         if(sc.next().equals("y")) {
-                            iturn = 0;
+                            iteration = 0;
                         } else {
-                            bGameIsOn = !bGameIsOn;
+                            beginGameIsOn = !beginGameIsOn;
                             break;
                         }
                     }
                 }
             }
-        }
+        }    	
+    }
+    
+    public static void main(String[] args) {
+
+        initiateDice();
+        playGame();
+
     }
 }
